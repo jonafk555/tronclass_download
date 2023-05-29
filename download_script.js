ad00a7b933461f5fad1c4c12d9bfab5343b15b3e
jonafk555 <75651364+jonafk555@users.noreply.github.com>
2023-05-29 21:07:12 +0800

    Update download_script.js

--- download_script.js
+++ download_script.js
@@ -1,21 +1,33 @@
 function downloadPDFFile() {
-  // Replace the URL below with the actual URL of the PDF file
-  // 把 {你檔案的網址} 刪掉，放入你檔案的網址
-  var url = '{你檔案的網址}';
-  
-  function urlDecode(encodedString) {
-  var decodedString = decodeURIComponent(encodedString);
-  return decodedString;
+  fetch('#page=1&zoom=auto,-22,549')
+    .then(response => response.text())
+    .then(htmlContent => {
+      // Create a temporary element to parse the HTML content
+      var tempElement = document.createElement('div');
+      tempElement.innerHTML = htmlContent;
+
+      // Find the link element in the HTML
+      var linkElement = tempElement.querySelector('a');
+
+      var linkURL = linkElement.href;
+
+      // Extract the link from the URL
+      var urlObj = new URL(linkURL);
+      var link = decodeURIComponent(urlObj.searchParams.get('file'));
+
+      console.log('Extracted Link:', link);
+
+      // Create a link element to trigger the download
+      var downloadLink = document.createElement('a');
+      downloadLink.href = linkURL;
+      downloadLink.download = 'downloaded.pdf'; // Specify the desired file name
+
+      // Programmatically trigger the download
+      downloadLink.click();
+    })
+    .catch(error => {
+      console.log('Failed to fetch the HTML content:', error);
+    });
 }
 
-  var file = urlDecode(url);
-  // Create a temporary link element
-  var link = document.createElement('a');
-  link.href = file;
-
-  // Programmatically click the link to trigger the download
-  link.click();
-}
-
-// Call the function to download the PDF file
 downloadPDFFile();
